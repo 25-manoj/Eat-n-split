4d646e119ae60150136abaec1f8b2e164ffde377
25-manoj <manojwebdevelop@gmail.com>
2024-03-06 12:54:03 +0530

    FormSplitBill calculation update

--- src/components/FormSplitBill.js
+++ src/components/FormSplitBill.js
@@ -1,19 +1,40 @@
+import { useState } from "react";
+
 export default function FormSplitBill({ selectedFriend }) {
+  const [bill, setBill] = useState("");
+  const [paidByUser, setPaidByUser] = useState("");
+  const paidByFriend = bill ? bill - paidByUser : "";
+  const [whoIsPaying, setWhoIsPaying] = useState("user");
   return (
     <form className="form-split-bill">
       <h2>Split the bill with {selectedFriend.name}</h2>
 
       <label>bill value</label>
-      <input type="text" />
+      <input
+        type="text"
+        value={bill}
+        onChange={(e) => setBill(Number(e.target.value))}
+      />
 
       <label>Your expence</label>
-      <input type="text" />
+      <input
+        type="text"
+        value={paidByUser}
+        onChange={(e) =>
+          setPaidByUser(
+            Number(e.target.value) > bill ? paidByUser : Number(e.target.value)
+          )
+        }
+      />
 
       <label>{selectedFriend.name} expence</label>
-      <input type="text" disabled />
+      <input type="text" value={paidByFriend} disabled />
 
       <label>who is paying the bill</label>
-      <select>
+      <select
+        value={whoIsPaying}
+        onChange={(e) => setWhoIsPaying(e.target.value)}
+      >
         <option value="user">you</option>
         <option value="friend">{selectedFriend.name}</option>
       </select>
